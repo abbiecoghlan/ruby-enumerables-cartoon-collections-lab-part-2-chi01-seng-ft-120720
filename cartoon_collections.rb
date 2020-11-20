140d7f19d34ca02f706b7ab1fb346c76a4f82863
Abbie Coghlan <abbie.coghlan@gmail.com>
2020-11-20 20:34:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,7 +28,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.find do |element|
     if valid_calls.include? element == true 
-    
+    return element
     end   
     
   end
